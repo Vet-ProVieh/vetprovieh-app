b89cc98e9bf1404d8496c4e50d7ac65fb42c072e
Tobias Münch <to.muench@muench-its.de>
2020-05-15 09:34:32 +0200

    Farmers create/update

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-const { generateSW } = require('rollup-plugin-workbox');
-const { injectManifest } = require('rollup-plugin-workbox');
+
+const {generateSW} = require('rollup-plugin-workbox');
+const {injectManifest} = require('rollup-plugin-workbox');
 import copy from 'rollup-plugin-copy'
 
 
 import {
-  terser
+    terser
 } from 'rollup-plugin-terser';
 
 // `npm run build` -> `production` is true
@@ -14,44 +15,56 @@ import {
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/app/main.js',
-  output: {
-    file: 'www/bundle.js',
-    format: 'iife', // immediately-invoked function expression — suitable for <script> tags
-    sourcemap: true
-  },
-  plugins: [
-    resolve(), // tells Rollup how to find date-fns in node_modules
-    commonjs(), // converts date-fns to ES modules
-    production && terser(), // minify, but only in production
-    copy({ // Copy HTML-Pages to Public Folder
-      targets: [
-        { src: 'src/assets/*', dest: 'www/assets' },
-        { src: 'src/pages/*', dest: 'www'},
-        { src: 'node_modules/bulma/*', dest: 'www/node_modules/bulma'} ,
-        { src: 'node_modules/bulma-pageloader/*', dest: 'www/node_modules/bulma-pageloader'} ,
-        { src: 'node_modules/@fortawesome/fontawesome-free/*', dest: 'www/node_modules/fontawesome'}
-      ]
-    }),
-    generateSW({
-      swDest: 'www/wb_service_worker.js',
-      globDirectory: 'www/',
-      globPatterns: '**/*.{js,css,html,ico,png}',
-      clientsClaim: true,
-      skipWaiting: true,
-      runtimeCaching: [{
-        urlPattern: /service|node_modules/,
-        handler: "CacheFirst",
-        options: {
-          cacheName: 'api',
-          expiration: {
-            maxEntries: 100,
-            maxAgeSeconds: 72 * 60 * 60
-          },
-          cacheableResponse: { statuses: [0, 200] },
-        }
-      }],
-      globIgnores: ['injectManifest_sw.js'],
-    })
-  ]
+    input: 'src/app/main.js',
+    output: {
+        file: 'www/bundle.js',
+        format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+        sourcemap: true
+    },
+    plugins: [
+        resolve(), // tells Rollup how to find date-fns in node_modules
+        commonjs(), // converts date-fns to ES modules
+        production && terser(), // minify, but only in production
+        copy({ // Copy HTML-Pages to Public Folder
+            targets: [
+                {src: 'src/assets/*', dest: 'www/assets'},
+                {src: 'src/pages/*', dest: 'www'},
+                {src: 'node_modules/bulma/*', dest: 'www/node_modules/bulma'},
+                {src: 'node_modules/bulma-pageloader/*', dest: 'www/node_modules/bulma-pageloader'},
+                {src: 'node_modules/@fortawesome/fontawesome-free/*', dest: 'www/node_modules/fontawesome'}
+            ]
+        }),
+        generateSW({
+            swDest: 'www/wb_service_worker.js',
+            globDirectory: 'www/',
+            globPatterns: ['**/*.{js,css,html,ico,png}'],
+            clientsClaim: true,
+            skipWaiting: true,
+            runtimeCaching: [{
+                urlPattern: /service/,
+                handler: "NetworkFirst",
+                options: {
+                    cacheName: 'api',
+                    expiration: {
+                        maxEntries: 100,
+                        maxAgeSeconds: 72 * 60 * 60
+                    },
+                    cacheableResponse: {statuses: [0, 200]},
+                }
+            },
+                {
+                    urlPattern: /node_modules/,
+                    handler: "CacheFirst",
+                    options: {
+                        cacheName: 'api',
+                        expiration: {
+                            maxEntries: 100,
+                            maxAgeSeconds: 72 * 60 * 60
+                        },
+                        cacheableResponse: {statuses: [0, 200]},
+                    }
+                }],
+            globIgnores: ['injectManifest_sw.js'],
+        })
+    ]
 };
