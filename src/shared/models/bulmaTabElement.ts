b9a1f028b3126a476db0d443d8909e3a61a31211
Tobias Münch <to.muench@muench-its.de>
2022-06-02 13:57:32 +0200

    Refaktoring tabs

---
+++ src/shared/models/bulmaTabElement.ts
@@ -0,0 +1,9 @@
+
+/**
+ * Tabulator Element
+ */
+export class BulmaTabElement {
+    public icon: string | undefined;
+    public name: string | undefined;
+    public displayName: string | undefined;
+}
